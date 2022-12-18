b6e3e751cc25c9ad3cde2f07e8ea1328a4fc5a27
marcelohcortez <marcelohcortez@gmail.com>
2022-12-18 16:02:14 +0000

    creat react app, create function to track click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const ClickedProps = [{
+  clientX: 0,
+  clientY: 0
+}]
+
 function App() {
+  const [ clickedPoints, setClickedPoints ] = useState(ClickedProps)
+  
+  function getCoordinates(event) {
+    const { clientX, clientY } = event
+  
+    setClickedPoints([...clickedPoints, { clientX, clientY }])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={getCoordinates}>
+      {clickedPoints.map( (clickedPoint) => {
+        return (
+          <div className='redDot' style={{
+            left: clickedPoint.clientX - 6,
+            top: clickedPoint.clientY - 7
+          }}></div>
+        )
+      })}
     </div>
   );
 }
